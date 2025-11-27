4371da019236d516c3e624ecfa54a5baf3b103e3
KrutikaChincholkar07 <chincholkarkrutika07@gmail.com>
2025-11-28 00:02:53 +0530

    Create PrimeNumber.java
    Print All Prime Numbers in a Range code in Java 
    
    Steps for checking prime:
    For each number n:
    If n < 2 → skip (not prime)
    Check divisibility from 2 to sqrt(n)
    If divisible → not prime
    If no divisors found → prime

---
+++ PrimeNumber.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PrimeRange {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter start of range: ");
+        int start = sc.nextInt();
+
+        System.out.print("Enter end of range: ");
+        int end = sc.nextInt();
+
+        System.out.println("Prime numbers in the range:");
+
+        for (int num = start; num <= end; num++) {
+            if (isPrime(num)) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        if (n < 2) return false;
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
